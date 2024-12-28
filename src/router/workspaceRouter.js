0f3498c28a9c018b98dbff202627b79b4862dcd6
RatneshMaurya1 <mauryaratnesh022@gmail.com>
2024-12-28 15:59:01 +0530

    Added update user api

--- src/router/workspaceRouter.js
+++ src/router/workspaceRouter.js
@@ -213,11 +213,18 @@ workspaceRouter.get('/share/workspace/:workspaceId', userAuth, async (req, res) 
       return res.status(404).json({ message: "Workspace not found." });
     }
 
+    if (workspace.owner.toString() === currentUser._id.toString()) {
+      return res.status(400).json({
+        message: "You cannot share the workspace with yourself.",
+      });
+    }
+
     if (!['view', 'edit'].includes(mode)) {
       return res.status(400).json({
         message: "Invalid mode. Mode must be 'view' or 'edit'.",
       });
     }
+
 
     const existingAccess = workspace.sharedWith.find(
       sharedUser => sharedUser.user.toString() === currentUser._id.toString()
